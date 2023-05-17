a957649844c8afc8d6bed396ab9ef2898d4a5bce
mukuljjjk <122144509+mukuljjjk@users.noreply.github.com>
2023-05-17 09:40:27 +0530

    Add files via upload

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React ,{useState} from 'react';
+import {GiHamburgerMenu} from "react-icons/gi"
+import {MdOutlineRestaurantMenu} from "react-icons/md"
+import './Navbar.css';
+import  images from "../../constants/images"
+
+const Navbar = () => {
+  const[toogleMenu,setToogleMenu]=useState(false)
+
+
+  return (
+  <div className='app__navbar'>
+    <div className='app__navbar-logo'>
+     <img src={images.gericht} alt="app_logo" />
+    </div>
+    <ul className='app__navbar-links'>
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+    </ul>
+    <div className='app__navbar-login'>
+      <a href='#login' className='p__opensans'>Log In / Registration</a>
+      <div />
+      <a href="/" className='p__opensans'>Book Table</a>
+    </div>
+    <div className='app__navbar-smallscreen'>
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={()=>{setToogleMenu(true)}} />
+
+
+     {toogleMenu && (
+      <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+      <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={()=>{setToogleMenu(false)}}/>
+       <ul className='app__navbar-smallscreen_links'>
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+       </ul>
+      </div>
+     ) }
+
+     
+    </div>
+
+
+  </div> )
+};
+
+export default Navbar;
